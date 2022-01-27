0c781cdcb468831a66d91f701521a662668372f0
GibbZ-78 <95296501+GibbZ-78@users.noreply.github.com>
2022-01-27 20:06:06 +0100

    Commit de backup darktrader+mairiechristmas 27 janvier 2022 20h05

--- lib/darktrader.rb
+++ lib/darktrader.rb
@@ -7,18 +7,66 @@ require 'nokogiri'
 # Useful global vars
 $url = "https://coinmarketcap.com/all/views/all/"
 
-
-
-
-
-# Main method starting the program and calling all others
+# Main method - Starting and organizaing the program and calling all others
 def darktrader
+  # Vars initialization
+  debug = "DEBUG > "
+  currency_name = []
+  currency_code = []
+  currency_value = []
+  my_crypto_hash = {}
+  my_crypto_tab = []
+
+  # HTML flow opening and loading, then filtering into 3 "Nodesets"
   page = Nokogiri::HTML(URI.open($url))
-  devises = page.xpath(('//td[@class="cmc-table__cell cmc-table__cell--sortable cmc-table__cell--left cmc-table__cell--hide-sm cmc-table__cell--sort-by__symbol"]'))
-  valorisations = page.xpath(('//a[@class="cmc-link"]'))
-  # puts devises
-  # puts "********"
-  puts valorisations
+  scrap_code = page.xpath("//*[@id='__next']/div[1]/div[2]/div/div[1]/div/div[2]/div[3]/div/table/tbody/*/td[2]/div/a[1]") #Exemple : BTC
+  scrap_name = page.xpath("//*[@id='__next']/div[1]/div[2]/div/div[1]/div/div[2]/div[3]/div/table/tbody/*/td[2]/div/a[2]") #Exemple : Bitcoin
+  scrap_value = page.xpath("//*[@id='__next']/div[1]/div[2]/div/div[1]/div/div[2]/div[3]/div/table/tbody/*/td[5]/div/a/span") #Exemple : 3656.45
+  
+  # DEBUG
+  # puts page.class
+  # puts scrap_code.class
+  # puts scrap_name.class
+  # puts scrap_value.class
+
+  #
+  scrap_name.each do |name|
+    currency_name.push(name.text)
+    # DEBUG
+    # puts debug+name.text
+  end
+
+  scrap_code.each do |code|
+    currency_code.push(code.text)
+    # DEBUG
+    # puts debug+code.text
+  end
+
+  scrap_value.each do |value|
+    currency_value.push(value.text.delete("$,").to_f)
+    # DEBUG
+    # puts debug+value.text
+  end
+
+  # DEBUG
+  # print currency_name
+  # puts "**************************"
+  # puts currency_code
+  # puts "**************************"
+  # puts currency_value
+  # puts "**************************"
+
+  for i in 0...currency_name.length do
+    # Old attempt... Did not get why it doesn't work yet
+    # my_crypto_hash = {currency_name[i].to_s.to_sym => currency_value[i]}
+    my_crypto_hash = Hash.new
+    my_crypto_hash[currency_code[i].to_sym] = [currency_name[i], currency_value[i]]
+    puts debug+"Code: #{currency_code[i]}  -  Name: #{currency_name[i]}  -  Value: #{currency_value[i]}"
+    my_crypto_tab.push(my_crypto_hash)
+  end
+
+  puts my_crypto_tab
+
 end
 
 # Invokation of the main method
--- lib/mairiechristmas.rb
+++ lib/mairiechristmas.rb
@@ -5,14 +5,41 @@ require 'open-uri'
 require 'nokogiri'
 
 # Useful global vars
-$url = "http://annuaire-des-mairies.com/"
+$url1 = "https://www.annuaire-des-mairies.com/val-d-oise.html"
+$url2 = "https://www.annuaire-des-mairies.com/95/"
 
+# get_townhall_mail - Given the URL of a given city, return the related mail address
+def get_townhall_mail(townhall_url)
+  page_townhall = Nokogiri::HTML(URI.open(townhall_url))
+  scrap_mail = page_townhall.xpath("/html/body/div/main/section[2]/div/table/tbody/tr[4]/td[2]").text
+  puts scrap_mail
+  return scrap_mail
+end
 
-
-
+# get_cities_urls - Given the base URL of a French department, return the URls of all related cities
+def get_cities_urls(base_url)
+  tab_cities_urls = []
+  page_base = Nokogiri::HTML(URI.open(base_url))
+  scrap_townhall = page_base.xpath("//*[@id='voyance-par-telephone']/table/tbody/tr[2]/td/table/tbody/tr/*/p/a")
+  scrap_townhall.each do |lien|
+    lien1 = lien.text
+    tab_cities_urls.push($url2+lien1.downcase.gsub(" ","-")) 
+  end
+  puts tab_cities_urls
+  return tab_cities_urls
+end
 
 # Main method starting the program and calling all others
 def mairiechristmas
+  
+  # get_cities_urls($url1).each do |city_url|
+  #   get_townhall_mail(city_url)
+  # end
+  puts
+  puts get_townhall_mail("https://www.annuaire-des-mairies.com/95/ableiges.html")
+  puts
+  puts get_cities_urls($url1)
+  puts
 
 end
 
